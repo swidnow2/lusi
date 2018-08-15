0c3585366c4a564076cc66cbd7c3bc756032553a
Esteban Casuscelli <ecasuscelli@Estebans-MacBook-Pro.local>
2018-08-15 11:08:54 +0200

    Changes to shell script to make it easier to run for all indexes directories

--- lusi.sh
+++ lusi.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
+#
+#Description: shell script to run lusi
+#Redirect stderr and stdout to the same file. Run it like this:  ./lusi.sh PATH &>outputfile.out
 
-java -jar bin/lusi-1.1-SNAPSHOT.jar $1 $2
+# Check command line args
+if [ $# -lt 1 ]; then
+	printf "\nNo arguments were passed to the script.\n"
+	printf "Usage: lusi.sh index_directory\n\n"
+	exit 1
+elif [ $# -gt 1 ]; then
+	printf "\nThe script requires only one argument. More than one arguments were passed.\n"
+	printf "Usage: lusi.sh index_directory\n\n"
+	exit 1
+fi
+
+# Check that is a directory and exists
+if [ ! -d $1 ]; then
+	printf "\nThe provided argument is not a directory or it does not exist\n\n"
+	exit 1
+fi
+
+# Check the string passed to ensure it has "/" at the end.
+if [[ "$1" == */ ]]; then
+	ls -1 $1>.dir_list.out
+	for a in `cat .dir_list.out`
+	do
+		java -jar bin/lusi-1.1-SNAPSHOT.jar $1$a
+	done
+else
+	ls -1 $1>.dir_list.out
+	for a in `cat .dir_list.out`
+	do
+		java -jar bin/lusi-1.1-SNAPSHOT.jar $1/$a
+	done
+fi
+
+rm -f .dir_list.out
